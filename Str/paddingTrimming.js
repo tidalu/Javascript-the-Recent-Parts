3a18280804e82186c992af774ac426c2efc10dbf
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-20 10:53:35 +0100

    solved: a tag function with padding and trimming

--- Str/paddingTrimming.js
+++ Str/paddingTrimming.js
@@ -22,3 +22,32 @@ var str1 = "    str str some stuff  \t\t";
 str.trim();  // "str str some stuff"
 str.trimStart(); // "str str some stuff          "
 str.trimEnd(); // "    str str some stuff"
+
+
+// problem with tag function with string pad trim
+
+function formatString(strings, ...values) {
+    var res = "";
+    for(let i = 0; i < strings.length; i++) {
+      res += strings[i];
+      if(i < values.length) {
+      if(typeof values[i] === "string") {
+        res += values[i].padStart(values[i].length + 3, " ").padEnd(values[i].length + 6, "!");
+      } else if (typeof values[i] === "number") {
+        let strNum = String(values[i]);
+        res += strNum.padStart(strNum.length+4, "->").padEnd(strNum.length+8, ".");
+      } else {
+        res += String(values[i])
+      }
+      }
+    }
+    return res
+  }
+  console.clear()
+  const name = 'John';
+  const age = 25;
+  const formattedString = formatString`Name: ${name}, Age: ${age}`;
+  console.log(formattedString);
+  // Expected Output:"Name:    John!!!, Age: ->->25...."
+  
+  
