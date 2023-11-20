9cb81c5264786e68f8d02816b8eb3daf32316962
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-20 10:21:42 +0100

    solved the template strings exercise

--- tasks/template-strings/ex.js
+++ tasks/template-strings/ex.js
@@ -1,10 +1,17 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	var res = ""; 
+	for(i = 0; i < strings.length; i++ ) {
+		res += strings[i];
+		values[i] ? res+=`${values[i].toUpperCase()}` : ""
+	}
+	return res
+}
 
 var name = "kyle",
 	twitter = "getify",
 	topic = "JS Recent Parts";
 
 console.log(
-	`Hello ____ (@____), welcome to ____!` ===
+	upper`Hello ${name} (@${twitter}), welcome to ${topic}!` ===
 	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
 );
