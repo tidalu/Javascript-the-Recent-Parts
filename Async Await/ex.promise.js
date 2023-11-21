2b3c5f6ae14bdddead9009d08bd5e595a7709443
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-21 14:42:29 +0100

    solved promise questions

--- Async Await/ex.promise.js
+++ Async Await/ex.promise.js
@@ -24,3 +24,31 @@ Promise.race([  // returns only returs the first done
 ]).then((message) => {
     console.log(message)
 })
+
+
+
+
+
+// wheather api function
+
+const fetchWeatherAPI1 = () => {
+    return new Promise((resolve, reject) => {
+        // simulating an async api call
+        setTimeout(() => {
+            const wheatherData = {
+                temprature: 25, 
+                condition: "sunny"
+            };
+
+            // simulating a 90% chnace of success
+            Math.random() < 0.9 ? resolve(wheatherData): resolve("APi 1 failed");
+        }, Math.random() * 3000); // random delay to simulate network latency
+    });
+};
+
+fetchWeatherAPI1()
+.then((wheatherData) => {
+    console.log(wheatherData)
+}).catch((error) => {
+    console.error(error)
+})
