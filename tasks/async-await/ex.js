470b9b7f141e5e2f2650397bb288e627d99c571a
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-21 15:11:52 +0100

    solved async problem of frontend masters

--- tasks/async-await/ex.js
+++ tasks/async-await/ex.js
@@ -6,8 +6,11 @@ function getFile(file) {
 
 async function loadFiles(files) {
 	// request all files concurrently
-
+	var prs = files.map(getFile)
 	// print in order, sequentially
+	for(let pr of prs){
+		console.log(await pr)
+	}
 }
 
 loadFiles(["file1","file2","file3"]);
