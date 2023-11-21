4cc79a277c8ef7c579e2dd0b9da2517980bfde1c
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-21 10:47:07 +0100

    solved iteration problem of frontend mas

--- Iterators & Generators/exercises.js
+++ Iterators & Generators/exercises.js
@@ -1,22 +1,19 @@
 var numbers = {
 	*[Symbol.iterator]({
-		start = 0, 
-		stop = 100, 
-		step = 1
+	  start= 1, 
+	  stop = 100, 
+	  step = 1
 	} = {}) {
-		for( let v = start; v <= stop; v+=step) {
-			yield v;
-		}
+	  for( start; start <= stop; start += step ) {
+		yield start
+	  }
 	}
-};
-
-// should print 0..100 by 1s
-for (let num of numbers) {
-	// console.log(num);
-}
-
-// should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
-
-// Hint:
-    console.log([...numbers[Symbol.iterator]( {start: 6, stop: 30, step: 4} )])
+  }
+  
+  for( let num of numbers) {
+	console.log(num)
+  }
+  
+  
+  console.log(`My luck numbers are: ${[...numbers[Symbol.iterator]({start: 6, stop: 30, step: 4})]}`)
+  
