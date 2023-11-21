3fd030e185cc7333f8d621869c9e4f757934d274
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-21 14:19:40 +0100

    solved some problems from the net for lookahead bk

--- Regular Expressions/lookAheadAndbehind.js
+++ Regular Expressions/lookAheadAndbehind.js
@@ -1,13 +1,16 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
 
-var msg = "Hello World";
+console.clear()
 
-console.log(msg.match(/(l.)/g));
-console.log(msg.match(/(l.)$/g));
-console.log(msg.match(/(l.)(?=o)/g));
-console.log(msg.match(/(l.)(?!o)/g)); // these fpur was look ahead , luckily we have now look behind
+const msg = "Hello World";
 
-// behind
+console.log(msg.match(/(l.)/g))
+console.log(msg.match(/(.l)/g))
+console.log(msg.match(/(l.)$/g))
+console.log(msg.match(/(l.)(?!o)$/g))
+console.log(msg.match(/(.l)(?=o)/g))
 
-console.log(msg.match(/(?<=e)(l.)/g)) 
-console.log(msg.match(/(?<!e)(l.)/g)) 
+console.log(msg.match(/(?<=e)(l.)/g))
+console.log(msg.match(/(?<=e)(l.)(?=o)/g))
+console.log(msg.match(/(?<!e)(l.)(?!o)/g))
+console.log(msg.match(/(?<!h)(l.)(?=o)/g))
