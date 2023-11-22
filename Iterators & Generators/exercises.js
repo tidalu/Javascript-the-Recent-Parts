843d9e8ceb1bc3b33fa5ce2692129294e22bc3d0
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-22 09:58:09 +0100

    solved one iteration with improvements

--- Iterators & Generators/exercises.js
+++ Iterators & Generators/exercises.js
@@ -17,3 +17,46 @@ var numbers = {
   
   console.log(`My luck numbers are: ${[...numbers[Symbol.iterator]({start: 6, stop: 30, step: 4})]}`)
   
+
+
+  // solved with next function attached to it 
+  var numbers1 = {
+	[Symbol.iterator]({
+	  start = 1, 
+	  stop = 100, 
+	  step = 1
+	} = {}) {
+	  return {
+		next: () => {
+		  if (start <= stop) {
+			const result = { done: false, value: start };
+			start += step;
+			return result;
+		  } else {
+			return { done: true, value: undefined };
+		  }
+		}
+	  }
+	}
+  }
+  
+  var nums = numbers1[Symbol.iterator]({
+	start: 12, 
+	stop: 50, 
+	step: 3
+  })
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
+	console.log(nums.next())
